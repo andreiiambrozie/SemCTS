e1af42a81658c97f929b11d5522d2505bea138a9
andreiiambrozie <72299212+andreiiambrozie@users.noreply.github.com>
2021-05-10 00:08:30 +0300

    facade x2?

--- Facade/src/ro/ase/acs/clase/BirouDeCredite.java
+++
@@ -1,8 +0,0 @@
-package ro.ase.acs.clase;
-
-class BirouDeCredite {
-    public static boolean areCredit(Persoana persoana){
-        return Integer.parseInt(""+persoana.getCNP().charAt(12))%2==0;
-
-    }
-}
