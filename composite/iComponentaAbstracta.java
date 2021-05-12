0f8463614139d708400e0823ca37259234c84e21
andreiiambrozie <72299212+andreiiambrozie@users.noreply.github.com>
2021-05-12 18:22:20 +0300

    Delete composite directory

--- composite/iComponentaAbstracta.java
+++
@@ -1,9 +0,0 @@
-package clase.composite;
-
-public interface iComponentaAbstracta {
-
-    public void descrie();
-    public void addNod(iComponentaAbstracta componenta) throws Exception;
-    public void removeNod(iComponentaAbstracta componenta)throws Exception;
-    public iComponentaAbstracta getNod(int position) throws Exception;
-}
