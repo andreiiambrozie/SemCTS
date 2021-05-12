bf80db89da687df03516488aaa7b718f3ee6049a
andreiiambrozie <72299212+andreiiambrozie@users.noreply.github.com>
2021-05-12 23:27:05 +0300

    +main

--- SubiectSpitalA/src/Main.java
+++ SubiectSpitalA/src/Main.java
@@ -1,17 +1,27 @@
 import clase.Decorator.*;
 import clase.adapter.Medicament;
 import clase.adapter.MedicamentAdapter;
+import clase.command.*;
+import clase.composite.Departament;
+import clase.composite.Sectie;
+import clase.composite.Spital;
+import clase.composite.SubDepartament;
 import clase.facade.Facade;
 import clase.facade.Pat;
 import clase.factoryMethod.FactoryAsistent;
 import clase.factoryMethod.FactoryInfirmier;
 import clase.factoryMethod.PersonalSpital;
 import clase.adapter.iMedicamentFaraReteta;
+import clase.flyweight.Flyweight;
+import clase.flyweight.FlyweightFactory;
+import clase.flyweight.Internare;
 import clase.prototype.Reteta;
+import clase.proxy.ProxyInternare;
 import clase.simpleFactory.SimpleFactory;
 import clase.simpleFactory.TipPersonal;
-import clase.builder.Pacient;
+import clase.Pacient;
 import clase.builder.PacientBuilder;
+import clase.strategy.PlataCard;
 
 public class Main {
 
@@ -91,5 +101,77 @@ public class Main {
 
         DecoratorAbstract decoratorPrintatFizic=new DecoratorPrintatFizic(new Rezultat("Ion",7));
         decoratorPrintatFizic.afisareRezultatDiferit();
+
+
+        //8
+
+        Sectie sectie=new Sectie("Sectie1");
+        SubDepartament sb1=new SubDepartament("Sb1");
+
+        Departament dep=new Departament("Dep1");
+        Departament dep2=new Departament("Dep2");
+
+        SubDepartament sb2=new SubDepartament("sb2");
+
+        Spital spital=new Spital();
+        spital.addNod(dep);
+        dep.addNod(sb2);
+        spital.addNod(dep2);
+        dep2.addNod(sb1);
+        dep2.addNod(sectie);
+
+
+
+        spital.descrie();
+
+        //9
+        pacient.setAreAsigurare(true);
+        ProxyInternare proxyInternare=new ProxyInternare(pacient);
+        proxyInternare.internareCuAsigurare(pacient);
+        proxyInternare.internareCuAsigurare(pacient1);
+
+        //10
+        Internare internare=new Internare("22",1,10);
+        FlyweightFactory flyweightFactory=new FlyweightFactory();
+        Pacient pacient2=new Pacient("vecinu","2222","ltilie");
+
+        Flyweight pacientInternat=flyweightFactory.getFlyweight("0000");
+        Flyweight pacientInternat2=flyweightFactory.getFlyweight("11111");
+        Flyweight pacientInternat3=flyweightFactory.getFlyweight("2222");
+        pacientInternat.afisareInternare(internare);
+        pacientInternat.afisareInternare(internare);
+        pacientInternat3.afisareInternare(internare);
+
+        //11
+        pacient.platesteInternare();
+        pacient.setTipDePlata(new PlataCard());
+        pacient.platesteInternare();
+
+
+        //12 memento?
+
+        //13 State
+        pacient.setStareInternat();
+        pacient.setStareSubObsv();
+        pacient.setStareExternat();
+
+        //14 template ??
+
+
+        //15
+        Medic medic1=new Medic("Ambrozie");
+        Comanda cInternare=new ComandaInternare(medic1);
+        Comanda cTratare=new ComandTratare((Medic) medic);
+        Comanda cInternare2=new ComandaInternare(medic1);
+
+        Operator operator=new Operator(); //manageriaza comenzile trimise
+
+        operator.addComanda(cTratare);
+        operator.addComanda(cInternare);
+        operator.addComanda(cInternare2);
+
+        operator.executaComanda();
+        operator.executaComanda();
+        operator.executaComanda();
     }
 }
